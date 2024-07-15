a4047da28dff8d828cf778e47312730cf352ba91
abdullah-afzal <mr.abdullahafzal@gmail.com>
2024-07-15 12:00:18 +0500

    [fix] fix webhook responce

--- src/api/store/tabby/route.ts
+++ src/api/store/tabby/route.ts
@@ -1,11 +1,11 @@
 import type {
     CartService,
-    MedusaRequest,
-    MedusaResponse,
+    // MedusaRequest,
+    // MedusaResponse,
     OrderService,
   } from "@medusajs/medusa";
   
-  export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
+  export const POST = async (req, res) => {
     //@ts-ignore
   const id= req.body.id;
     
@@ -14,14 +14,14 @@ import type {
     try {
       const order = await orderService.retrieveByCartId(id);
       res
-        .status(302)
-        .redirect(`${process.env.cmi_store_home}/order/${order.id}`);
+        .status(200)
+        .redirect(`${process.env.tabby_store_home}/order/${order.id}`);
     } catch (error) {
       try {
         const ca = await cartService.authorizePayment(id);
         const data = await orderService.createFromCart(ca.id);
         res
-          .status(302)
+          .status(200)
           .redirect(`${process.env.tabby_store_home}/order/${data.id}`);
       } catch (err) {
         console.error("Error completing order:", err);
